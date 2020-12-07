a5c2130c32549e311af611d09f73558db2575ea7
ngocdu <ngocdu171@gmail.com>
2020-12-07 14:49:15 +0200

    ready for heroku

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 
 const connection = new Pool({
-  user: 'dupham',
-  host: 'localhost',
-  database: 'resort_db',
-  password: '1234567',
+  user: 'jhstrlqovdwrjo',
+  host: 'ec2-54-170-123-247.eu-west-1.compute.amazonaws.com',
+  database: 'dd6dteuo1kp416',
+  password: '96bddc77dab95c632984c11c3e2d76f13e16e176848ad10cca54340d2dcd3e59',
   port: 5432,
 })
 module.exports = connection;
